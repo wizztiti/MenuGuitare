2210e07344a44b18a235c636c8019b9d03e24bd5
ndelcast.dev@gmail.com <ndelcast.dev@gmail.com>
2015-12-14 19:30:11 +0100

    Modif Nico :)

--- breadcrumb.js
+++ breadcrumb.js
@@ -1,12 +1,16 @@
 var breadcrumb = {
 	init: function () {
-		var widthMenu = $(".breadcrumb li:last-child").width() - 22;
+
+		var widthMenu = $(".breadcrumb .with-menu").width() - 22;
 		var widthLink = $(".breadcrumb .dropDown a").width();
+
 		var decalageMenu = widthLink - widthMenu;
-		$(".breadcrumb .dropDown a").css({"min-width": widthMenu + "px"});
+		$(".breadcrumb .dropDown a").css({
+			"min-width": widthMenu + "px"
+		});
 		//$(".breadcrumb .dropDown").css({left : decalageMenu});
 
- 		$(".breadcrumb li:last-child").mouseenter( function(){
+ 		$(".breadcrumb .with-menu").mouseenter( function(){
 	      $(".breadcrumb .dropDown").show();
 	    }).mouseleave(function(){
 	      $(".breadcrumb .dropDown").hide();
